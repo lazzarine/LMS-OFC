a30d2c79faf9a0ed003b322f14e152716b4acc3c
Renato de souza lucas <rlucas.rs2@gmail.com>
2018-11-08 21:16:29 -0200

    Atualização das demais urls

--- OFC-LMS/firetek/core/urls.py
+++ OFC-LMS/firetek/core/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     url (r'^$', views.index, name='index'),
     url (r'^curso1/', views.curso1, name='curso1'),
     url (r'^curso2/', views.curso2, name='curso2'),
+    url (r'^usr/', views.usr, name='usr'),
+    url (r'^login/', views.login, name='login'),
+    url (r'^table/', views.table, name='table'),
+    url (r'^cadasto/', views.cadastro, name='cadastro'),
+    url (r'^calendar/', views.calendar, name='calendario'),
    
 ]
--- OFC-LMS/firetek/firetek/urls.py
+++ OFC-LMS/firetek/firetek/urls.py
@@ -5,8 +5,13 @@ from core.views import *
 
 urlpatterns = [
   url(r'^admin/', admin.site.urls),
-    url(r'^core/', include('core.urls')),
-    url(r'^curso1/', include('core.urls')),
-    url(r'^curso2/', include('core.urls')),
+  url(r'^core/', include('core.urls')),
+  url(r'^curso1/', include('core.urls')),
+  url(r'^curso2/', include('core.urls')),
+  url(r'^usr/', include('core.urls')),
+  url(r'^login/', include('core.urls')),
+  url(r'^table/', include('core.urls')),
+  url(r'^cadastro/', include('core.urls')),
+  url(r'^calendar/', include('core.urls')),
     
 ]
